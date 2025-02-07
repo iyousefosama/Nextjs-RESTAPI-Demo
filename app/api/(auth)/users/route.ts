d7fd337e98bb5b839b6d93ba9424c6279a7502de
WOLF <blackwolf3466@gmail.com>
2025-02-07 17:59:04 +0200

    Added comments on users api routes and db.ts

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,15 +1,18 @@
+// * Mongodb connect function
 import connect from "@/lib/db";
+// * Users model(Schema) for mongodb 
 import Users from "@/lib/models/user";
 import {NextResponse} from "next/server";
 
+// * Mongoose ObjectId type for typescript
 const ObjectId = require("mongoose").Types.ObjectId;
 
 export async function GET() {
     try {
-        await connect();
-        const users = await Users.find();
+        await connect(); // Executes connect function
+        const users = await Users.find(); // Get all data users
 
-        return NextResponse.json(users, {status: 200});
+        return NextResponse.json(users, {status: 200}); // Send json response with all found users
     } catch (error: any) {
         return NextResponse.json(
             {error: "💢 Error while fetching users: " + error.message},
@@ -22,10 +25,17 @@ export async function GET() {
 
 export async function POST(request: Request) {
     try {
-        const body = await request.json();
-        await connect();
-        const newUser = await new Users(body).save();
+        /**
+         * User data should contain:
+         * * String email
+         * * String username
+         * * String password
+         */
+        const body = await request.json(); // Get the request body in json format
+        await connect(); // Executes connect function
+        const newUser = await new Users(body).save(); // Creates a new db User with the body and saves it
 
+        // Send a response with created user data and a success message
         return NextResponse.json(
             {
                 user: newUser,
@@ -45,11 +55,12 @@ export async function POST(request: Request) {
 
 export async function PATCH(request: Request) {
     try {
-        const body = await request.json();
-        const {userId, newUsername} = body;
+        const body = await request.json(); // Get request body as json
+        const {userId, newUsername} = body; // Get userId & newUsername values from the json body
 
-        await connect();
+        await connect(); // Executes the connect function
 
+        // If userID & newUsername not found in body return response error message
         if (!userId || !newUsername) {
             return NextResponse.json(
                 {error: "💢 Missing required fields"},
@@ -57,20 +68,24 @@ export async function PATCH(request: Request) {
             );
         }
 
+        // Checks if userId is a valid mongodb ObjectId
         if (!ObjectId.isValid(userId)) {
             return NextResponse.json({error: "💢 Invalid user ID"}, {status: 400});
         }
 
+        // Finds the user by id and updates it's username
         const user = await Users.findOneAndUpdate(
             {_id: new ObjectId(userId)},
             {username: newUsername},
-            {new: true}
+            {new: true} // returns the new data
         );
 
+        // If the user were not found returns error message response
         if (!user) {
             return NextResponse.json({error: "💢 User not found!"}, {status: 404})
         }
 
+        // If everything is fine return success response message
         return NextResponse.json(
             {message: "✅ Successfully updated user"},
             {status: 200}
@@ -85,9 +100,10 @@ export async function PATCH(request: Request) {
 
 export async function DELETE(request: Request) {
     try {
-        const {searchParams} = new URL(request.url);
-        const userId = searchParams.get("userId");
+        const {searchParams} = new URL(request.url); // Get the searchParams from the request url
+        const userId = searchParams.get("userId"); // Get the "userId" from searchParams
 
+        // If user not found returns error message response
         if (!userId) {
             return NextResponse.json(
                 {error: "💢 userId is required!"},
@@ -95,17 +111,22 @@ export async function DELETE(request: Request) {
             );
         }
 
+        // Check if userId is a valid ObjectId
         if (!ObjectId.isValid(userId)) {
             return NextResponse.json({error: "💢 Invalid user ID"}, {status: 400});
         }
 
-        await connect();
+        await connect(); // Executes connect function
 
+        // Finds the user and deletes it
         const user = await Users.findOneAndDelete({_id: new ObjectId(userId)});
+        
+        // If user not found returns response error message
         if (!user) {
             return NextResponse.json({error: "💢 User not found!"}, {status: 404});
         }
 
+        // success message
         return NextResponse.json(
             {message: "✅ Successfully deleted user", user: user},
             {status: 200}
--- lib/db.ts
+++ lib/db.ts
@@ -1,20 +1,30 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI; // The mongoDb connection string
 
+// Connect function that will be executed each time in the requests
 const connect = async () => {
+    /**
+     * Checks the mongodb readyState
+     * 0 = disconnected
+     * 1 = connected
+     * 2 = connecting
+     */
     const mongoConnection = mongoose.connection.readyState;
 
+    // Don't do anything on connected state
     if (mongoConnection === 1) {
         // * MongoDb is already connected.
         return;
     }
 
+    // Logs "connecting" and do nothing on connecting state
     if (mongoConnection === 2) {
         console.log("🔄 [MONGO]: Connecting...");
         return;
     }
 
+    // If state not connected or connecting tries to connect
     try {
         await mongoose.connect(MONGO_URI!);
         console.log("✅ [MONGO]: Connected!");
